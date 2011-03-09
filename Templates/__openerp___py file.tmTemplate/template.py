b85fb9dadc733e382dbed54ca9cd8dc6048a56ff
debird <debird@Mac-mini-VincenzoA.local>
2011-03-09 16:11:26 +0100

    important fix __openerp__.py template

--- Templates/__openerp___py file.tmTemplate/template.py
+++ Templates/__openerp___py file.tmTemplate/template.py
@@ -1,24 +1,22 @@
-#!/usr/bin/env python
 # encoding: utf-8
-"""
-${TM_NEW_FILE_BASENAME}.py
 
-Created by ${TM_FULLNAME} on ${TM_DATE}.
-Copyright (c) ${TM_YEAR} ${TM_ORGANIZATION_NAME}. All rights reserved.
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+# untitled.py
+# 
+# Created by debird on 2011-03-09.
+# Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+# 
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+# 
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+# 
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {
     "name": "name",
     "version": "1.0",
